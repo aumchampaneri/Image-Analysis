6ab2988ff17dbad365ebb4857dcec0705ed71a84
aumchampaneri <aum.champaneri@outlook,com>
2023-09-29 16:32:44 -0400

    General updating

--- 20230928 Image Luminosity/Luminosity Analysis.py
+++
@@ -1,7 +0,0 @@
-import cv2 as cv
-from matplotlib import pyplot as plt
-
-img = cv.imread("03_Control_Calcium_20x_0000.tiff", cv.IMREAD_GRAYSCALE)
-assert img is not None, "File not found/read"
-
-#
---
+++ Luminosity Analysis.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+# Image import - 1 for now
+#TODO fix image import issue
+img = cv.imread('Test Images/03_Control_Calcium_20x_0001.tiff', cv.IMREAD_GRAYSCALE)
+assert img is not None, "File not found/read"
+# TODO add remaining files
+
+'''
+Pipeline:
+1. Otsu Thresholding
+2. Mask Whiie Cells against original image
+3. Pixel Intensity Plot per image
+    3a. Average image intensity per group
+4. Apply to the 40x images
+'''
+
+# Otsu Thresholding
+ret1, th1 = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+
+# Show Result
+cv.imshow('Otsu', th1)
